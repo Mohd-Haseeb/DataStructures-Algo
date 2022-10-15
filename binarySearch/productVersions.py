4f8652bac13b7bc93773576c51cb8c27aba24e15
Mohd-Haseeb <mohd.haseeb440@gmail.com>
2022-10-15 21:57:25 +0530

    productversion.py solver

--- binarySearch/productVersions.py
+++ binarySearch/productVersions.py
@@ -1,5 +1,34 @@
-# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad. Suppose you have n version and you want to find out the first bad one, which causes all the following ones to be bad. Also, talk about the time complexity of your code.
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check.
+#  Since each version is developed based on the previous version, all the versions after a bad version are also bad. Suppose you have n version and
+#  you want to find out the first bad one, which causes all the following ones to be bad. Also, talk about the time complexity of your code.
 # Test Cases:
 # Input: [0,0,0,1,1,1,1,1,1]
 # Output: 3
 # Explanation: 0 indicates a good version and 1 indicates a bad version. So, the index of the first 1 is at 3. Thus, the output is 3
+
+
+def badVersion(arr):
+    start = 0
+    end = len(arr) -1 
+
+    while start <= end:
+        mid = start + (end-start)//2
+
+        if arr[mid] == 1 and arr[mid-1] == 0:
+            return mid
+        
+        if arr[mid]==0:
+            start = mid + 1
+        
+        if arr[mid]==1 and arr[mid-1]==1:
+            end = mid -1
+
+    return -1
+
+
+# DRIVER CODE
+if __name__ == "__main__":
+    a = [0,0,0,1,1,1,1,1]
+    ans = badVersion(a)
+
+    print(ans)
